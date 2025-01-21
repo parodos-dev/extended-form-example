9ebb18cdff94308faa0cc58f86eecd5232b707a2
Marek Libra <marek.libra@gmail.com>
2025-01-21 08:39:02 +0100

    Fix typescript

--- plugins/custom-form-example-plugin/src/customApi.tsx
+++ plugins/custom-form-example-plugin/src/customApi.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FormDecoratorProps, OrchestratorFormApi } from '@janus-idp/backstage-plugin-orchestrator-form-api';
-import { ErrorSchema, FormValidation, RegistryWidgetsType, UiSchema } from '@rjsf/utils';
+import { ErrorSchema, FieldErrors, FormValidation, RegistryWidgetsType, UiSchema } from '@rjsf/utils';
 import {JsonObject} from '@backstage/types';
 import { JSONSchema7 } from 'json-schema';
 import CountryWidget from './widgets/CountryWidget';
 import LanguageWidget from './widgets/LanguageSelectWidget';
 import { FormContextData } from './types';
 
-type Data = {
+interface Data extends JsonObject {
   personalInfo: {
     country: string;
     firstName: string;
@@ -25,35 +25,46 @@ const reservedNames = ['admin', 'root', 'system'];
 const sleep = (ms: number) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+const customValidate = (formData: JsonObject | undefined, errors: FormValidation<Data>): FormValidation<JsonObject> => {
+  const _formData = formData as Data | undefined;
+  if (_formData?.personalInfo?.password !== _formData?.personalInfo?.confirmPassword) {
+    errors.personalInfo?.password?.addError("passwords do not match.");  
+  }          
+  return errors;
+}         
+
 class CustomFormExtensionsApi implements OrchestratorFormApi {  
-    getFormDecorator(_schema: JSONSchema7, _uiSchema: UiSchema<JsonObject>, initialFormData?: JsonObject) {
+    getFormDecorator(_schema: JSONSchema7, _uiSchema: UiSchema<JsonObject>, initialFormData?: Data) {
       return (FormComponent: React.ComponentType<FormDecoratorProps>) => {    
         return () => {
           const [formContext, setFormContext] = React.useState<FormContextData>({country: initialFormData?.personalInfo?.country});
-          const customValidate = (formData: JsonObject | undefined, errors: FormValidation<JsonObject>): FormValidation<JsonObject> => {
-            const _formData = formData as Data | undefined;
-            if (_formData?.personalInfo?.password !== _formData?.personalInfo?.confirmPassword) {
-              errors.personalInfo?.password?.addError("passwords do not match.");  
-            }          
-            return errors;
-          }         
+
+        
           const widgets: RegistryWidgetsType<JsonObject, JSONSchema7, any> = {
               LanguageWidget, 
               CountryWidget,
           };
+
+          const onChange = (data: Data) => {
+            console.log("on change");
+            if (data.personalInfo?.country !== formContext.country ) {
+              setFormContext({country: data.personalInfo?.country});                    
+            }                  
+          };
+
           return (<FormComponent widgets={widgets} onChange={(e) => {
-              const data = e.formData as Data;            
-              console.log("on change");
-              if (data.personalInfo?.country !== formContext.country ) {
-                setFormContext({country: data.personalInfo?.country});                    
-              }                  
+              const data = e.formData as Data;
+              onChange(data);
             }} 
             formContext={formContext}            
             customValidate={customValidate}
             getExtraErrors={async (formData: JsonObject) => {
               const _formData = formData as Data;
+
               return sleep(1000).then(() => { 
                 const errors: ErrorSchema<Data> = {};
+
                 if (reservedNames.includes(_formData.personalInfo?.firstName)) {
                   errors.personalInfo = {
                     firstName: {
